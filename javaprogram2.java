0c64dba32e6cf0a4b83ded6741ea5ba056ec3a3e
unknown <sbhagya685@gmail.com>
2024-10-05 06:12:51 +0530

    adding all class

---
+++ javaprogram2.java
@@ -0,0 +1,14 @@
+class patternprogram
+{
+public static void main (String []args)
+{
+for(int i=1;i<=100;i++)
+{
+for(int j=1;j<=100;j++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
---
+++ patternprogram1.java
@@ -0,0 +1,14 @@
+class patternprogram1
+{
+public static void main(String[]args)
+{
+for(int i=1; i>=10; i++)
+{
+for(int j=1; j<=8; j++)
+{
+System.out.print("*");
+}
+System.out.println("");
+}
+}
+}
